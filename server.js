bfa6fdb9139398f6c7a256c302a4201468207da2
Manish <md663744@gmail.com>
2021-11-08 00:52:39 +0530

    Updated  port

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ io.on('connection', socket =>{
 
 
 
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000 
 
 server.listen(PORT,()=>console.log(`Server running on port http://localhost:${PORT}`))
 
